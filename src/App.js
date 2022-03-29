4939de6bbd65b2026640643767ee5b16b4589ae2
Dennis Diaz <Diaz3737@gmail.com>
2022-03-29 18:39:08 -0400

    HTML/CSS done.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Quote from "./Quote";
+
+function App() {
+  return (
+    <>
+      <Quote />
+      <footer className="links">footer</footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Quote.jsx
@@ -0,0 +1,11 @@
+
+const Quote = () => {
+  return (
+    <div className="App">
+        <h1 className="quote-text">"Talent is a pursued interest. Anything that you’re willing to practice, you can do."</h1>
+        <h3 className="author">- Bob Ross</h3>
+      </div>
+  )
+}
+
+export default Quote
