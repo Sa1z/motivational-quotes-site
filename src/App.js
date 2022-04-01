fc1748526bdb2073e50c4bad365dea35639f3ad3
Dennis Diaz <Diaz3737@gmail.com>
2022-03-31 20:14:03 -0400

    Added api. Resolve CORS issue.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import { useEffect, useState } from "react";
 import Quote from "./Quote";
 import Button from "./Button";
 
+const API_URL = 'https://zenquotes.io/api/random'
+
 function App() {
+
+  const searchQuotes = async (url) => {
+    const response = await fetch(url)
+    const data = await response.json();
+
+    console.log(data)
+  }
+
+
+
+  useEffect(() => {
+    searchQuotes(API_URL);
+  }, [])
+
   return (
-    <>
+    <div className="App">
       <Quote />
       <Button />
-      <footer className="links">&copy; 2022</footer>
-    </>
+      <footer className="links">Inspirational quotes provided by <a href="https://zenquotes.io/" target="_blank">ZenQuotes API</a></footer>
+    </div>
   );
 }
 
